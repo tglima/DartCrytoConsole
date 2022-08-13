dc511252510a5e11a125c6061c3635d4eaaeae06
tglima <thg.limadesousa@gmail.com>
2022-08-13 18:29:18 -0300

    Refactor tests

--- test/dart_cryto_console_test.dart
+++ test/dart_cryto_console_test.dart
@@ -1,8 +1,36 @@
 import 'package:test/test.dart';
-import 'package:dart_cryto_console/app.dart';
+import 'package:dart_cryto_console/app.dart' as app;
 
 void main() {
-  test('checkIsOK', () {
-    expect(checkIsOK(), 'OK');
+  test('cryptDefaultKey', () {
+    List<String> args = ['-t', 'Efraim'];
+    expect(app.run(args), 0);
+  });
+
+  test('cryptDefaultKeyWithOptionExplicit', () {
+    List<String> args = ['-t', 'Efraim', '-o', 'e'];
+    expect(app.run(args), 0);
+  });
+
+  test('deCryptDefaultKey', () {
+    List<String> args = ['-t', 'Dcz7NXQ6VF8=', '-o', 'd'];
+    expect(app.run(args), 0);
+  });
+
+  test('crypPersonaltKey', () {
+    List<String> args = ['-t', 'leaoJuda', '-k', 'Pr30:5CaPaDeusCoPura'];
+    expect(app.run(args), 0);
+  });
+
+  test('deCryptPersonaltKey', () {
+    List<String> args = [
+      '-t',
+      'JmYLptNNWj87Ws4JgAELaA==',
+      '-o',
+      'd',
+      '-k',
+      'Pr30:5CaPaDeusCoPura'
+    ];
+    expect(app.run(args), 0);
   });
 }
